186729f00b9ec4caa867c0a4024a3b8b24f167c5
Fabien Collet <fbncollet@gmail.com>
2017-09-24 01:21:46 +0200

    asteroid - improve

--- asteroid/main.lua
+++ asteroid/main.lua
@@ -10,6 +10,7 @@ width = 16
 win = false
 playerTab = {}
 playerTab[1] = {
+    shot=0,
     size=16,
     x=50,
     y=50,
@@ -20,6 +21,7 @@ playerTab[1] = {
     color={43, 168, 226, 255}
     }
 playerTab[2] = {
+    shot=0,
     size=16,
     x=1100,
     y=700,
@@ -44,6 +46,8 @@ bulletTab = {}
 bulletTab[1] = {}
 bulletTab[2] = {}
 
+gameOverImg = love.graphics.newImage( '/assets/images/game_over.png' )
+gameOverWidth, gameOverHeight = gameOverImg:getDimensions()
 function tablelength(T)
   local count = 0
   for _ in pairs(T) do count = count + 1 end
@@ -83,12 +87,15 @@ function love.update(dt)
     for id, player in pairs(bodyTab) do
         angle = player.body:getAngle()
 
+        playerTab[1]['shot'] = playerTab[1]['shot'] + 1
+
         linVX, linVY = player.body:getLinearVelocity( )
         if tablelength(joystickTab) >= id then
             direction1 = joystickTab[id]:getAxis( 1 )
             direction2 = joystickTab[id]:getAxis( 2 )
             direction3 = joystickTab[id]:getAxis( 3 )
             dashAxis = joystickTab[id]:getGamepadAxis( 'triggerright' )
+            brakeAxis = joystickTab[id]:getGamepadAxis( 'triggerleft' )
         else
             direction1 = 0
             direction2 = 0
@@ -113,21 +120,6 @@ function love.update(dt)
             love.event.quit( "restart" )
         end
 
-        if joystickTab[id]:isGamepadDown( 'a' ) then
-            win = true
-            player.body:setX(playerTab[id]['x'])
-            player.body:setY(playerTab[id]['y'])
-            player.body:setLinearVelocity(0,0)
-            player.body:setAngularVelocity(0)
-            player.body:setAngle(0)
-        end
-
-        if dashAxis > 0.05 then
-            dash = 1+(dashAxis*10)
-        else
-            dash = 1
-        end
-
         posX = player.body:getX()
         posY = player.body:getY()
 
@@ -138,32 +130,23 @@ function love.update(dt)
         end
 
         
-
-        if joystickTab[id]:isGamepadDown('rightshoulder') then
-            degree = angle + math.rad(90)
-            forceX = math.cos( degree )
-            forceY = math.sin( degree )
-            bulletShape = love.physics.newRectangleShape( forceX*16, forceY*16, 3, 6, angle )
-            bulletBody = love.physics.newBody( world, posX, posY, 'dynamic' )
-            bulletFixture = love.physics.newFixture( bulletBody, bulletShape, 1 )
-            
-            bulletBody:applyForce(forceX*50, forceY*50)
-            if tablelength(bulletTab[id]) > 100 then
-                table.remove( bulletTab[id], 1 )
-            end
-            table.insert( bulletTab[id], {body = bulletBody,shape = bulletShape, fixture= bulletFixture} )
-        end
-
-        for i, bullet in pairs(bulletTab[id]) do
-            if not id == i then
-                distance, x1, y1, x2, y2 = love.physics.getDistance( player.fixture, bullet.fixture )
-                if distance <= 0 then
-                    playerWin = id
-                    win = true
+        if playerTab[1]['shot'] > 33 then
+            if joystickTab[id]:isGamepadDown('rightshoulder') then
+                playerTab[1]['shot'] = 0
+                degree = angle + math.rad(90)
+                forceX = math.cos( degree )
+                forceY = math.sin( degree )
+                bulletShape = love.physics.newRectangleShape( forceX*16, forceY*16, 3, 6, angle )
+                bulletBody = love.physics.newBody( world, posX, posY, 'dynamic' )
+                bulletFixture = love.physics.newFixture( bulletBody, bulletShape, 1 )
+                
+                bulletBody:applyForce(forceX*50, forceY*50)
+                if tablelength(bulletTab[id]) > 20 then
+                    table.remove( bulletTab[id], 1 )
                 end
+                table.insert( bulletTab[id], {body = bulletBody,shape = bulletShape, fixture= bulletFixture} )
             end
         end
-        
 
         for id, bullet in pairs(bulletTab[id]) do
             if bullet.body:getX() > windowWidth then
@@ -178,32 +161,58 @@ function love.update(dt)
             end
         end
 
+        for i, bullet in pairs(bulletTab[id]) do
+            if id == 1 then
+                distance, x1, y1, x2, y2 = love.physics.getDistance( bodyTab[2].fixture, bullet.fixture )
+                if distance <= 0 then
+                    playerWin = id
+                    win = true
+                end
+            elseif id == 2 then
+                distance, x1, y1, x2, y2 = love.physics.getDistance( bodyTab[1].fixture, bullet.fixture )
+                if distance <= 0 then
+                    playerWin = id
+                    win = true
+                end
+            end
+            
+        end
+        
+
         if posY > windowHeight then
             player.body:setY(0)
         elseif posY < 0 then
             player.body:setY(windowHeight-playerTab[id]['size'])
         end
 
-        if direction1 > 0.1 or direction1 < -0.1 then
+
+        degree = angle + math.rad(90)
+        forceX = math.cos( degree )
+        forceY = math.sin( degree )
+
+        if dashAxis > 0.05 then
             if linVX < playerTab[id]['maxVelocity'] and linVX > playerTab[id]['maxVelocity']*-1 then
                 -- character.x = character.x + ((character.speed*dash) * direction1) * dt
-                player.body:applyForce(((playerTab[id]['speed']*dash) * direction1) * dt , 0)
+                player.body:applyForce(((playerTab[id]['speed'] * forceX) * dt) * dashAxis , 0)
             end
-        elseif linVX > 0 then
-            player.body:applyForce(-worldTab.friction, 0)
-        elseif linVX < 0 then
-            player.body:applyForce(worldTab.friction, 0)
-        end
-        if direction2 > 0.1 or direction2 < -0.1 then
+        
             if linVY < playerTab[id]['maxVelocity'] and linVY > playerTab[id]['maxVelocity']*-1 then
                 -- character.y = character.y + ((character.speed*dash) * direction2) * dt
-                player.body:applyForce(0, ((playerTab[id]['speed']*dash) * direction2) * dt)
+                player.body:applyForce(0, ((playerTab[id]['speed'] * forceY) * dt) * dashAxis)
             end
-        elseif linVY > 0 then
-            player.body:applyForce(0, -worldTab.friction)
-        elseif linVY < 0 then
-            player.body:applyForce(0, worldTab.friction)
         end
+        if linVX > 0 then
+            player.body:applyForce(-worldTab.friction-brakeAxis*2, 0)
+        elseif linVX < 0 then
+            player.body:applyForce(worldTab.friction+brakeAxis*2, 0)
+        end
+
+        if linVY > 0 then
+            player.body:applyForce(0, -worldTab.friction-brakeAxis*2)
+        elseif linVY < 0 then
+            player.body:applyForce(0, worldTab.friction+brakeAxis*2)
+        end
+        
 
         if direction3 > 0.1 or direction3 < -0.1 then
             angle = angle + ((5) * direction3) * dt
@@ -212,11 +221,12 @@ function love.update(dt)
     end
 end
 
+
 -- Draw a coloured rectangle.
 function love.draw()
     if win==false then
-        love.graphics.setColor(255, 255, 255, 255)
-        love.graphics.print(tostring( distance ), 800)
+        --love.graphics.setColor(255, 255, 255, 255)
+        --love.graphics.print(tostring( playerTab[1]['shot'] ), 800)
 
         for id, player in pairs(bodyTab) do
             love.graphics.setColor(playerTab[id]['color'])
@@ -229,6 +239,7 @@ function love.draw()
             end
         end
     else
-        love.graphics.print('YOU WIN!! '..tostring(playerWin), 900, 500, 0,2,2)
+        -- love.graphics.print('YOU WIN!! '..tostring(playerWin), 900, 500, 0,2,2)
+        love.graphics.draw(gameOverImg, windowWidth/2-(gameOverWidth/2), windowHeight/2-(gameOverHeight/2),0,1,1)
     end
 end
